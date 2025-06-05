8295ef13207d3cad17266a3ffc2c3a6872f78774
Gwangkyo <rhkdqhr08@gmail.com>
2025-06-05 11:06:13 +0900

    feat: CORS

---
+++ cors1/src/main/java/org/example/cors1/IndexController.java
@@ -0,0 +1,13 @@
+package org.example.cors1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ cors1/src/test/java/org/example/cors1/Cors1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cors1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cors1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
