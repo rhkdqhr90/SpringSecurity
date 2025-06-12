7534d31cffd51778d69889da36a9c2daa6efe29e
Gwangkyo <rhkdqhr08@gmail.com>
2025-06-12 17:34:32 +0900

    feat: RequestMatcherDelegationgAuthorizationManager인가설정

--- src/main/java/srpingsecurity/security/SecurityConfig.java
+++ src/main/java/srpingsecurity/security/SecurityConfig.java
@@ -5,6 +5,9 @@ package srpingsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +18,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEntry;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @EnableWebSecurity
@@ -27,11 +38,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user").hasRole("USER")
-                        .requestMatchers("/db").access(new WebExpressionAuthorizationManager("hasRole('DB')"))
-                        .requestMatchers("/admin").hasAnyAuthority("ADMIN")
-                        .requestMatchers("/secure").access(new CustomAuthorizationManager())
-                        .anyRequest().authenticated())
+//                        .requestMatchers("/user").hasRole("USER")
+//                        .requestMatchers("/db").access(new WebExpressionAuthorizationManager("hasRole('DB')"))
+//                        .requestMatchers("/admin").hasAnyAuthority("ADMIN")
+//                        .requestMatchers("/secure").access(new CustomAuthorizationManager())
+
+                        .anyRequest().access(authorizationManager(null )))
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
@@ -41,6 +53,31 @@ public class SecurityConfig {
 //    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
 //        return new GrantedAuthorityDefaults("MYPREFIX_");
 //    }
+
+
+    @Bean
+    public AuthorizationManager<RequestAuthorizationContext> authorizationManager(HandlerMappingIntrospector introspector) {
+        List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings = new ArrayList<>();
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry1 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector,"/user"), AuthorityAuthorizationManager.hasAnyAuthority("USER"))
+                ;
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry2 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector,"/db"), AuthorityAuthorizationManager.hasAnyAuthority("DB"))
+                ;
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry3 =
+                new RequestMatcherEntry<>(new MvcRequestMatcher(introspector,"/admin"), AuthorityAuthorizationManager.hasAnyAuthority("ADMIN"))
+                ;
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry4 =
+                new RequestMatcherEntry<>(AnyRequestMatcher.INSTANCE, new AuthenticatedAuthorizationManager<>());
+        mappings.add(requestMatcherEntry1);
+        mappings.add(requestMatcherEntry2);
+        mappings.add(requestMatcherEntry3);
+        mappings.add(requestMatcherEntry4);
+
+        return new CustomRequestMatcherDelegatingAuthorizationManager(mappings);
+
+    }
 
 
 
