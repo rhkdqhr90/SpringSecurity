01c9cb989cc0e2ef8ecfd5e7a66e42ab52f4083a
Gwangkyo <rhkdqhr08@gmail.com>
2025-06-13 12:44:24 +0900

    feat: servlet,MVC, 비동기 통합

--- src/main/java/srpingsecurity/security/IndexController.java
+++ src/main/java/srpingsecurity/security/IndexController.java
@@ -2,35 +2,61 @@ package srpingsecurity.security;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
 
 @RestController
 @RequiredArgsConstructor
 public class IndexController {
 
-    private final DataSource dataSource;
+    AuthenticationTrustResolverImpl trustResolver= new AuthenticationTrustResolverImpl();
 
     @GetMapping("/")
-    public Authentication index(Authentication authentication) {
-        return authentication;
+    public String index() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return trustResolver.isAnonymous(auth) ? "anonymous" : "authenticated";
     }
 
 
     @GetMapping("/user")
-    public String user() {
-        return dataSource.getUser();
+    public User user(@AuthenticationPrincipal User user) {
+        return user;
     }
 
-    @GetMapping("/db")
-    public Account oauth(@RequestParam String name) {
-        return dataSource.getOwner(name);
+
+    @GetMapping("/currentUser")
+    public User currentUser(@CurrentUser User user) {
+        return user;
     }
 
-    @GetMapping("/secure")
-    public String secure() {
-        return "secure";
+
+
+    @GetMapping("/currentUsername")
+    public String currentUser2(@CurrentUsername String user) {
+        return user;
+    }
+
+    @GetMapping("/callable")
+    public Callable<Authentication> callable() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        System.out.println("securityContext: " + securityContext);
+        System.out.println("parent Thread : " + Thread.currentThread().getName());
+        return new Callable<Authentication>() {
+            @Override
+            public Authentication call() {
+                SecurityContext securityContext = SecurityContextHolder.getContext();
+                System.out.println("securityContext: " + securityContext);
+                System.out.println("child Thread : " + Thread.currentThread().getName());
+                return securityContext.getAuthentication();
+            }
+        };
     }
 }
