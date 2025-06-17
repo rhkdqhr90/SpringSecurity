6e53a5b2a13aa21b0dfc4ee5e8e55db966acb8c1
Gwangkyo <rhkdqhr08@gmail.com>
2025-06-17 19:17:51 +0900

    feat: 커스텀 인증 실패 핸들러

--- src/main/java/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ src/main/java/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -8,8 +8,11 @@ import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import security.springsecuritymaster.security.details.FormWebAuthenticationDetailsSource;
+import security.springsecuritymaster.security.handler.FormAuthenticationFailureHandler;
 import security.springsecuritymaster.security.handler.FromAuthenticationSuccessHandler;
 import security.springsecuritymaster.security.provider.FormAuthenticationProvider;
 
@@ -20,16 +23,18 @@ public class SecurityConfig {
 
     private final FormAuthenticationProvider authenticationProvider;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> formWebAuthenticationDetailsSource;
-    private final FromAuthenticationSuccessHandler fromAuthenticationSuccessHandler;
+    private final AuthenticationSuccessHandler successHandler;
+    private final AuthenticationFailureHandler failureHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth ->auth
                 .requestMatchers("/css/**","/images/**","/js/**","/favicon.*/","/*/icon-*").permitAll()
-                .requestMatchers("/","/signup").permitAll()
+                .requestMatchers("/","/signup","/login*").permitAll()
                 .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login").permitAll()
                         .authenticationDetailsSource(formWebAuthenticationDetailsSource)
-                        .successHandler(fromAuthenticationSuccessHandler))
+                        .successHandler(successHandler)
+                        .failureHandler(failureHandler))
                 .authenticationProvider(authenticationProvider);
 
         return http.build();
